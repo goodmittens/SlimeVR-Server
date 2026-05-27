4dbb01bcbb3cffb711d8b874369ee6682e67c832
Sapphire <imsapphire0@gmail.com>
2026-05-27 00:59:12 -0500

    Fix magnetometer status not changing when restarting HID tracker

--- server/core/src/main/java/dev/slimevr/tracking/trackers/Device.kt
+++ server/core/src/main/java/dev/slimevr/tracking/trackers/Device.kt
@@ -2,6 +2,7 @@ package dev.slimevr.tracking.trackers
 
 import dev.slimevr.tracking.trackers.udp.BoardType
 import dev.slimevr.tracking.trackers.udp.MCUType
+import dev.slimevr.tracking.trackers.udp.MagnetometerStatus
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.delay
@@ -41,7 +42,7 @@ open class Device(val magSupport: Boolean = false) {
 	 * if `sensorId` null or in the specified tracker
 	 * @param sensorId If null, every sensor will be modified
 	 */
-	open suspend fun setMag(state: Boolean, sensorId: Int = 255) {
+	open suspend fun setMag(status: MagnetometerStatus, sensorId: Int = 255) {
 		TODO("Not implemented because no mag support: $magSupport")
 	}
 
